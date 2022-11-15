5bc72cdd6c91dd93c602e5552d5f96a26eba1e55
Anton <Krasikov-94@bk.ru>
2022-11-15 17:51:39 +0300

    Initial Commit

---
+++ WeatherKrasikov/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherKrasikov
+//
+//  Created by Антон Красиков on 15.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
